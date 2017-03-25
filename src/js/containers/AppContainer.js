0a26c1932a0fc015203a3a8282e09178b2111ff3
yami_beta <yami-beta@users.noreply.github.com>
2017-03-25 13:56:26 +0900

    Change App to AppContainer
    AppコンポーネントをAppContainerとして実装

--- src/js/containers/AppContainer.js
+++ src/js/containers/AppContainer.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Container } from 'flux/utils';
+import TodoList from '../components/TodoList';
+import TodoStore from '../stores/TodoStore';
 
 class App extends React.Component {
+  // Containerで扱うStoreを列挙する
+  static getStores() {
+    return [
+      TodoStore
+    ];
+  }
+
+  // StoreをStateと対応付ける
+  static calculateState() {
+    return {
+      todos: TodoStore.getState(),
+    };
+  }
+
   render() {
     return (
       <div>
         <p>Flux Async Todo Example</p>
+        <TodoList {...this.state}/>
       </div>
     )
   }
 }
 
-export default App;
+export default Container.create(App);
